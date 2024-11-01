a432e79b7e2c3f427edf15a3c896e9ecb3278faa
Eduardo Lima <eduardo.limaoliveira.50@gmail.com>
2024-11-01 15:37:06 -0400

    feat: criado um escopo para permitir login apenas de usuários de uma UO especifica

---
+++ app/Ldap/Scopes/OnlyAghuseUsers.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Ldap\Scopes;
+
+use LdapRecord\Models\Model;
+use LdapRecord\Models\Scope;
+use LdapRecord\Query\Model\Builder;
+
+class OnlyAghuseUsers implements Scope
+{
+    /**
+     * Apply the scope to the given query.
+     *
+     * @param Builder $query
+     * @param Model   $model
+     *
+     * @return void
+     */
+    public function apply(Builder $query, Model $model): void
+    {
+        $query->in('OU=AGHUSE,OU=HOSPITAIS,DC=aghuse,DC=int');
+
+        // You can also make this "environment aware" if needed:
+        // $query->in(env('LDAP_USER_SCOPE'));
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Ldap\Scopes\OnlyAghuseUsers;
 use Illuminate\Support\ServiceProvider;
+use App\Ldap\Scopes\OnlyAccountingUsers;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->register();
+
+        
     }
 }
